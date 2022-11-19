424dde7b090bbc755a7d14c98965f781117dc113
AlexNemtsev <alex.nemtsev95@gmail.com>
2022-11-19 21:14:28 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alex Nemtsev");
+        System.out.println("10.07.1995");
+
+    }
+}
