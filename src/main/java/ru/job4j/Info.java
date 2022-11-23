6c072d35d7910b482098c3ff431c4e972559931c
AlexNemtsev <alex.nemtsev95@gmail.com>
2022-11-23 20:52:19 +0300

    Info

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class Info {
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
         System.out.println("19.11.2022");
 
