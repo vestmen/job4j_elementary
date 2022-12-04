374762eb22c429518066b5d2f9263eb01b91e2e4
AlexNemtsev <alex.nemtsev95@gmail.com>
2022-12-04 11:03:36 +0300

    6

--- src/main/java/ru/job4j/math/Language.java
+++ src/main/java/ru/job4j/math/Language.java
@@ -2,6 +2,6 @@ package ru.job4j.math;
 
 public class Language {
     public static void main(String[] args) {
-        System.out.println("Размер вложенного массива");
+        System.out.println("Р Р°Р·РјРµСЂ РІР»РѕР¶РµРЅРЅРѕРіРѕ РјР°СЃСЃРёРІР°");
     }
 }
